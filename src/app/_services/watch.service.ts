89e34e911423e974b0ec33244c7c9d4dd7fec098
zreptil <andi@zreptil.de>
2023-04-19 17:28:49 +0200

    # added missing components

---
+++ src/app/_services/watch.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Utils} from '@/classes/utils';
+import {GLOBALS, GlobalsData} from '@/_model/globals-data';
+import {WatchElement} from '@/_model/watch-element';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WatchService {
+
+  constructor() {
+  }
+
+  get now(): Date {
+    return GlobalsData.now;
+  }
+
+  get isEditMode(): boolean {
+    return this.selected != null || (Utils.isEmpty(GLOBALS.watchList) ?? true);
+  }
+
+  get selected(): WatchElement {
+    return GLOBALS.watchList?.find((e) => e.selected);
+  }
+
+  get selectedIndex(): number {
+    return GLOBALS.watchList?.findIndex((e) => e.selected) ?? -1;
+  }
+
+}
---
+++ src/app/components/watch-group/watch-group.component.ts
@@ -0,0 +1,126 @@
+import {Component, Input} from '@angular/core';
+import {DataService} from '@/_services/data.service';
+import {ThemeService} from '@/_services/theme.service';
+import {SessionService} from '@/_services/session.service';
+import {GLOBALS, GlobalsData} from '@/_model/globals-data';
+import {WatchElement} from '@/_model/watch-element';
+import {Settings} from '@/_model/settings';
+import {WatchService} from '@/_services/watch.service';
+
+@Component({
+  selector: 'app-watch-group',
+  templateUrl: './watch-group.component.html',
+  styleUrls: ['./watch-group.component.scss']
+})
+export class WatchGroupComponent {
+  @Input()
+  groupId: string;
+
+  constructor(public ds: DataService,
+              public ts: ThemeService,
+              public ss: SessionService,
+              public ws: WatchService) {
+  }
+
+  get globals(): GlobalsData {
+    return GLOBALS;
+  }
+
+  get maxGluc(): number {
+    return Math.max(
+      Settings.stdHigh ?? 0,
+      GLOBALS.targetTop ?? 0,
+      GLOBALS.currentGlucValue ?? 0,
+      (GLOBALS.lastGlucValue ?? 0) + 5
+    );
+  }
+
+  get styleTargetLow(): any {
+    return {width: `calc(100%*${GLOBALS.targetBottom}/${this.maxGluc})`};
+  }
+
+  get styleTargetNorm(): any {
+    return {width: `calc(100%*${GLOBALS.targetTop - GLOBALS.targetBottom}/${this.maxGluc})`};
+  }
+
+  get styleTargetHigh(): any {
+    return {width: `calc(100%*${this.maxGluc - GLOBALS.targetTop}/${this.maxGluc})`};
+  }
+
+  get styleCurrentGluc(): any {
+    return {left: `calc(100%*${GLOBALS.currentGlucValue ?? 0}/${this.maxGluc})`};
+  }
+
+  get styleLastGluc(): any {
+    return {left: `calc(100%*${GLOBALS.lastGlucValue ?? 0}/${this.maxGluc})`};
+  }
+
+  get styleArrowTip(): any {
+    if ((GLOBALS.currentGlucValue ?? 0) === (GLOBALS.lastGlucValue ?? 0)) {
+      return {display: 'none'};
+    }
+    const x = (GLOBALS.currentGlucValue ?? 0) / this.maxGluc;
+    if ((GLOBALS.currentGlucValue ?? 0) < (GLOBALS.lastGlucValue ?? 0)) {
+      return {transform: 'rotate(135deg)', left: `calc(100%*${x} + 2px)`};
+    }
+    return {transform: 'rotate(-45deg)', left: `calc(100%*${x} - 12px)`};
+  }
+
+  get styleArrowTrack(): any {
+    const len = (GLOBALS.currentGlucValue ?? 0) - (GLOBALS.lastGlucValue ?? 0);
+    if ((GLOBALS.currentGlucValue ?? 0) < (GLOBALS.lastGlucValue ?? 0)) {
+      return {left: `calc(100%*${GLOBALS.currentGlucValue ?? 0}/${this.maxGluc})`, width: `calc(100%*${-len}/${this.maxGluc})`};
+    }
+    return {left: `calc(100%*${GLOBALS.lastGlucValue ?? 0}/${this.maxGluc})`, width: `calc(100%*${len}/${this.maxGluc})`};
+  }
+
+  get classForRoot(): string [] {
+    const ret = ['groupRoot', this.groupId];
+    if (this.groupId === 'center') {
+      ret.push(...this.classForGroup);
+    }
+    return ret;
+  }
+
+  get classForGroup(): string[] {
+    const ret = [];
+    const prefix = GLOBALS.isWatchColor ? 'color-' : 'dark-';
+    ret.push(`${prefix}${this.colForGluc(GLOBALS.currentGlucValue)}`);
+    return ret;
+  }
+
+  colForGluc(gluc: number): string {
+    if (gluc == null) {
+      return 'norm';
+    }
+    if (gluc < GLOBALS.targetBottom) {
+      return 'low';
+    } else if (gluc > GLOBALS.targetTop) {
+      return 'high';
+    }
+    return 'norm';
+  }
+
+  onClick(element: WatchElement) {
+    const value = !element.selected;
+    for (const entry of GLOBALS.watchList) {
+      entry.selected = false;
+    }
+    element.selected = value;
+  }
+
+  changeImage(entry: WatchElement): string {
+    const id = entry.type?.substring(7);
+    return `assets/img/${GLOBALS.currentChanges?.[id]?.type ?? 'empty'}.print.png`;
+  }
+
+  changeClass(entry: WatchElement): string[] {
+    const id = entry.type.substring(7);
+    return ['alarm', `alarm${GLOBALS.currentChanges?.[id]?.alarm}`];
+  }
+
+  changeInfo(entry: WatchElement): string {
+    const id = entry.type.substring(7);
+    return GLOBALS.currentChanges?.[id]?.lasttime;
+  }
+}
