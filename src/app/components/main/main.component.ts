665a7925273e334d4724cc402e3aaa9b9a0ddb48
Andi <andi@zreptil.de>
2023-07-17 13:51:42 +0200

    + added a text to feedback when language is not german or english

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -320,6 +320,13 @@ export class MainComponent implements OnInit {
           subject: $localize`Feedback zu Nightscout Reporter ${GLOBALS.version}`,
           body: `${GLOBALS.user.name}%0d----%0dURL zur Nightscout Instanz:%0d${GLOBALS.user.urlDataFor(new Date()).fullUrl('', '', true)}%0d----%0d`
         };
+        const regex = new RegExp('(de\-|en-).*', 'g');
+        if (!regex.test(GLOBALS.language.code)) {
+          const msg = 'Please note: I, the developer, can only understand German or English. '
+            + 'Please use one of these languages for communication. '
+            + 'This will make it easier for both of us.';
+          params.body = `${msg}%0d%0d${params.body}`;
+        }
         const paramList: string[] = [];
         for (const key of Object.keys(params)) {
           paramList.push(`${key}=${params[key]}`)
